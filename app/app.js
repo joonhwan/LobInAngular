a13a683c6a818edbf1d7a698538617abc67ab503
joonhwan <jhlee@joonhwanui-MacBook-Pro.local>
2016-05-26 23:30:46 +0900

    initial commit

---
+++ app/app.js
@@ -0,0 +1,8 @@
+/**
+ * Created by jhlee on 2016. 5. 26..
+ */
+
+(function() {
+    "use strict";
+    var app = angular.module("productManagement", []);
+}());
---
+++ app/products/productListCtrl.js
@@ -0,0 +1,35 @@
+/**
+ * Created by jhlee on 2016. 5. 26..
+ */
+(function() {
+    "use strict";
+    angular
+        .module("productManagement")
+        .controller("productListCtrl",  ProductListCtrl);
+
+    function ProductListCtrl() {
+        var vm = this;
+        vm.products = [
+            {
+                "productName": "iMac",
+                "productCode": "APP-MAC-011",
+                "releaseData": "2016/12/11",
+                "imageUrl":"https://www.apple.com/pr/products/images/iMac27_iMac21_Photos_PRINT_131020_HERO.jpg",
+                "price":1980
+            },
+            {
+                "productName": "Surface",
+                "productCode": "MSC-SFC-042",
+                "releaseDate": "2015/12/122",
+                "imageUrl": "http://cdn2.pcadvisor.co.uk/cmsdata/reviews/3364791/Microsoft_Surface_RT.jpg",
+                "price":2150
+            }
+        ];
+
+        vm.showImage = true;
+
+        vm.toggleImage = function() {
+            vm.showImage = !vm.showImage;
+        }
+    }
+}());
