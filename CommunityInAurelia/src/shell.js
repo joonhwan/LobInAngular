c037de591e025126132e42763a574e4ae263b58a
joonhwan <joonhwan.lee@gmail.com>
2016-06-02 12:42:57 +0900

    routing basics

--- CommunityInAurelia/src/shell.js
+++ CommunityInAurelia/src/shell.js
@@ -2,4 +2,23 @@ export class Shell {
     constructor() {
         
     }
+    
+    configureRouter(config, router) {
+        this.router = router;
+        config.title = "Joonhwan .NET User Group";
+        config.map([
+            { 
+                route: ['', 'events'], moduleId:'events/events', 
+                name:'Events', title:'EventsTitle', nav:true 
+            },
+            {
+                route: 'jobs', moduleId:'jobs/jobs',
+                name:'Jobs', title:'JobsTitle', nav:true
+            },
+            {
+                route: 'discussion', moduleId:'discussion/discussion',
+                name:'Discussion', title:'DiscussionTitle', nav:true
+            },
+        ]);
+    }
 }
