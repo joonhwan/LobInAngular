b67ab46894f3a478851e3e3fb77613479933018d
joonhwan <joonhwan.lee@gmail.com>
2016-06-03 12:59:47 +0900

    deactivate 시에 confirm 하는 기능 추가.

--- CommunityInAurelia/src/discussion/discussion.js
+++ CommunityInAurelia/src/discussion/discussion.js
@@ -1,5 +1,42 @@
+function getDiscussionInput() {
+    // fake data access
+    return "";
+}
+
+function cloneObject(obj) {
+    return JSON.parse(JSON.stringify(obj));
+}
+
 export class Discussion {
+    
     constructor() {
-        
     }
+    
+    activate() {
+        this.discussionInput = getDiscussionInput();
+        this.originalInput = cloneObject(this.discussionInput);
+    }
+    
+    save() {
+        // simulate save
+        this.originalInput = cloneObject(this.discussionInput);
+    }
+    
+    canDeactivate() {
+        if(JSON.stringify(cloneObject(this.discussionInput)) != 
+            JSON.stringify(this.originalInput)) {
+            if(confirm("Unsaved data, are you sure to navigate away?")) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    }
+    
+    deactivate() {
+        console.log("Discussion : Deactivated");
+    }
+    
 }
