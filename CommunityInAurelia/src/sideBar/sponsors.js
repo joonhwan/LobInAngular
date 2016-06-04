b23b38258a682667e16593485aa023311aa4b911
joonhwan <joonhwan.lee@gmail.com>
2016-06-04 09:05:31 +0900

    event.trigger, timer, ...

--- CommunityInAurelia/src/sideBar/sponsors.js
+++ CommunityInAurelia/src/sideBar/sponsors.js
@@ -1,5 +1,32 @@
+import {activationStrategy} from 'aurelia-router';
+
 export class Sponsors {
+    
     constructor() {
-        
+        this.message = "Sponsor 1";
+        this.counter = 1;
     }
+    
+    activate() {
+        this.intervalId = setInterval(() => {
+            // console.log('this : ' + this + this==window ? '(window)' : '');
+            // console.log('this.counter : ' + this.counter);
+            // console.log('this.message : ' + this.message);
+            this.message = 'Sponsor' + this.counter++;
+        }, 1000); 
+    }
+    
+    deactivate() {
+        clearInterval(this.intervalId);
+        this.counter = 1; // reset
+    }
+    
+    determineActivationStrategy() {
+        return activationStrategy.invokeLifecycle;
+    }
+    
+    doSomething(message) {
+        console.log(message);
+    }
+    
 }
