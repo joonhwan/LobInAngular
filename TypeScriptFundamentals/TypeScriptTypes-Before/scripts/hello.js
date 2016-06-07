94c5c48076f19400827b7fa0991660c1d24518ec
joonhwan <joonhwan.lee@gmail.com>
2016-06-07 09:34:05 +0900

    initial commit of typescript fundamentals course

---
+++ TypeScriptFundamentals/TypeScriptTypes-Before/scripts/hello.js
@@ -0,0 +1,18 @@
+﻿var Car = (function () {
+    function Car(engine) {
+        this.engine = engine;
+    }
+    Car.prototype.start = function () {
+        alert('Engine Started : ' + this.engine);
+    };
+    Car.prototype.stop = function () {
+        alert('Engine Stopped : ' + this.engine);
+    };
+    return Car;
+})();
+
+window.onload = function () {
+    var car = new Car('v8');
+    car.start();
+    car.stop();
+};
---
+++ TypeScriptFundamentals/TypeScriptTypes-Before/scripts/hello.ts
@@ -0,0 +1,18 @@
+﻿class Car {
+    constructor(public engine: string) {
+        
+    }
+
+    start() {
+        alert('Engine Started : ' + this.engine);
+    }
+    stop() {
+        alert('Engine Stopped : ' + this.engine);
+    }
+}
+
+window.onload = () => {
+    var car = new Car('v8');
+    car.start();
+    car.stop();
+}
