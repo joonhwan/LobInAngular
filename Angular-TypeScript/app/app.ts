b11ccd052234211bfab570ae598ba086d6b72440
joonhwan <joonhwan.lee@gmail.com>
2016-06-07 20:41:07 +0900

    updated Angular-TypeScript

--- Angular-TypeScript/app/app.ts
+++ Angular-TypeScript/app/app.ts
@@ -1,4 +1,20 @@
 module app {
-    angular.module("productManagement", 
-        ['common.services', 'productResourceMock']);
+    var main =
+        angular.module("productManagement", 
+            ['ngRoute', 'common.services', 'productResourceMock']);
+            
+    main.config(routeConfig);
+    
+    routeConfig.$inject = ['$routeProvider'];
+    function routeConfig($routeProvider:ng.route.IRouteProvider) : void {
+        $routeProvider
+            .when('/productList', { 
+                templateUrl: '/app/products/productListView.html',
+                controller: 'ProductList as vm',
+            })
+            .when('/product/:producdtId', {
+                templateUrl: '/app/products/'
+            }
+            ;
+    }
 }
