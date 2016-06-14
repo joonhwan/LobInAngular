762d599e3eb538435113eb040f732ee505096383
joonhwan <joonhwan.lee@gmail.com>
2016-06-14 23:48:31 +0900

    angula material float-action-button has been created.

--- angular-material-jspm/app/dist/controllers/mainController.js
+++ angular-material-jspm/app/dist/controllers/mainController.js
@@ -6,11 +6,12 @@ System.register([], function(exports_1, context_1) {
         setters:[],
         execute: function() {
             MainController = (function () {
-                function MainController(userService, $mdSidenav, $mdToast) {
+                function MainController(userService, $mdSidenav, $mdToast, $mdDialog) {
                     var _this = this;
                     this.userService = userService;
                     this.$mdSidenav = $mdSidenav;
                     this.$mdToast = $mdToast;
+                    this.$mdDialog = $mdDialog;
                     this.selectedUser = null;
                     this.searchText = '';
                     this.tabIndex = 0;
@@ -41,13 +42,42 @@ System.register([], function(exports_1, context_1) {
                         this.openToast(toastMessage);
                     }
                 };
+                MainController.prototype.removeAllNotes = function ($event) {
+                    this.showConfirm("Are you sure you want to delete all notes?", "Anoo notes will be deleted.");
+                    var confirm = this.$mdDialog.confirm()
+                        .title("Are sure you want to delete all notes?")
+                        .textContent("All notes will be deleted. You won't undo this action.")
+                        .targetEvent($event)
+                        .ok("Yes")
+                        .cancel("No");
+                    var self = this;
+                    this.$mdDialog.show(confirm).then(function () {
+                        self.selectedUser.notes = [];
+                        self.openToast("Deleted all notes.");
+                    });
+                };
+                MainController.prototype.showConfirm = function (title, text) {
+                    // Appending dialog to document.body to cover sidenav in docs app
+                    var confirm = this.$mdDialog.confirm()
+                        .title('Would you like to delete your debt?')
+                        .textContent('All of the banks have agreed to forgive you your debts.')
+                        .ariaLabel('Lucky day')
+                        .targetEvent(ev)
+                        .ok('Please do it!')
+                        .cancel('Sounds like a scam');
+                    this.$mdDialog.show(confirm).then(function () {
+                        status = 'You decided to get rid of your debt.';
+                    }, function () {
+                        status = 'You decided to keep your debt.';
+                    });
+                };
                 MainController.prototype.openToast = function (toastMessage) {
                     this.$mdToast.show(this.$mdToast.simple()
                         .textContent(toastMessage)
                         .position("top right")
                         .hideDelay(3000));
                 };
-                MainController.$inject = ["userService", "$mdSidenav", "$mdToast"];
+                MainController.$inject = ["userService", "$mdSidenav", "$mdToast", "$mdDialog"];
                 return MainController;
             }());
             exports_1("MainController", MainController);
--- angular-material-jspm/app/src/controllers/mainController.ts
+++ angular-material-jspm/app/src/controllers/mainController.ts
@@ -1,13 +1,15 @@
 //export { MainController };
 import {IUserService} from "../services/userService";
 import {User, Note} from "../model";
+import angular from 'angular';
 
 export class MainController {
-  static $inject = ["userService", "$mdSidenav", "$mdToast"];
+  static $inject = ["userService", "$mdSidenav", "$mdToast", "$mdDialog"];
   constructor(
     private userService:IUserService,
     private $mdSidenav: angular.material.ISidenavService,
-    private $mdToast: angular.material.IToastService
+    private $mdToast: angular.material.IToastService,
+    private $mdDialog: angular.material.IDialogService
     ) {
     this.userService.loadAllUsers()
       .then((users) => {
@@ -31,7 +33,7 @@ export class MainController {
     }
     this.tabIndex = 0;
   }
-  removeNote(note:Note) {
+  removeNote(note:Note) : void {
     var noteIndex = this.selectedUser.notes.indexOf(note);
     if(noteIndex >= 0) {
       this.selectedUser.notes.splice(noteIndex, 0);
@@ -40,6 +42,40 @@ export class MainController {
       //this.$mdToast.showSimple(toastMessage);
       this.openToast(toastMessage);
     }
+  }
+  removeAllNotes($event) : void {
+
+    this.showConfirm("Are you sure you want to delete all notes?", 
+      "Anoo notes will be deleted.");
+    var confirm = this.$mdDialog.confirm()
+      .title("Are sure you want to delete all notes?")
+      .textContent("All notes will be deleted. You won't undo this action.")
+      .targetEvent($event)
+      .ok("Yes")
+      .cancel("No")
+      ;
+      var self = this;
+      this.$mdDialog.show(confirm).then(() => {
+        self.selectedUser.notes = [];
+        self.openToast("Deleted all notes.")
+        ;
+      })
+  }
+
+  showConfirm(title:string, text:string) {
+    // Appending dialog to document.body to cover sidenav in docs app
+    var confirm = this.$mdDialog.confirm()
+          .title('Would you like to delete your debt?')
+          .textContent('All of the banks have agreed to forgive you your debts.')
+          .ariaLabel('Lucky day')
+          .targetEvent(ev)
+          .ok('Please do it!')
+          .cancel('Sounds like a scam');
+    this.$mdDialog.show(confirm).then(function() {
+      status = 'You decided to get rid of your debt.';
+    }, function() {
+      status = 'You decided to keep your debt.';
+    });
   }
 
   openToast(toastMessage:string): void {
