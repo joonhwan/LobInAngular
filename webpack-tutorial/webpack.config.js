b0617fc4dbcc66f4bd3b3b011a84ac5804c0d4d0
joonhwan <joonhwan.lee@gmail.com>
2016-06-19 21:16:26 +0900

    build를 별도폴더에 하도록 수정

--- webpack-tutorial/webpack.config.js
+++ webpack-tutorial/webpack.config.js
@@ -1,10 +1,17 @@
+var path = require('path');
+
 module.exports = {
+  context: path.resolve('js'),
   entry: {
     app: ['./app.js', './utils.js']
   },
   output: {
-    path: __dirname,
+    path: path.resolve('build/js'),
+    publicPath: '/public/assets/js',
     filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: 'public'
   },
   module: {
     preLoaders: [
