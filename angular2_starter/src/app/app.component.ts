d71ce82a519d766614cb04e95d7e1374470e4728
joonhwan <joonhwan.lee@gmail.com>
2016-06-20 23:03:47 +0900

    fininshed typscript+angular2+webpack

--- angular2_starter/src/app/app.component.ts
+++ angular2_starter/src/app/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: '<h1>은서야 사랑해</h1>'
+  template: '<h1>서연아, 은서야 사랑해.</h1>'
 })
 export class AppComponent {
   
