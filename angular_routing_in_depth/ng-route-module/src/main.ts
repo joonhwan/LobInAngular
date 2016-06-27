61146e61c7988acb6204a3363c722d8a4ef74db4
joonhwan <joonhwan.lee@gmail.com>
2016-06-28 06:20:46 +0900

    ngRoute-in-depth courser 추가

---
+++ angular_routing_in_depth/ng-route-module/src/main.ts
@@ -0,0 +1,24 @@
+import 'jquery';
+import 'lodash';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css';
+import * as angular from 'angular';
+import 'angular-route';
+
+let app = angular.module('ngroute-test', ['ngRoute']);
+
+app.config(['$logProvider', '$routeProvider', 
+  ($logProvider:ng.ILogProvider, $routeProvider:ng.route.IRouteProvider) => {
+    $logProvider.debugEnabled(true);
+
+    $routeProvider
+      .when('/', {
+        controller: function() {
+          this.message = "Hello Root Page";
+        },
+        controllerAs: 'vm',
+        template: '<h2>Root Page({{vm.message}})</h2>'
+      });
+  }])
+
+console.log('app initialized');
---
+++ angular_routing_in_depth/ng-route-module/typings/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="globals/angular-route/index.d.ts" />
+/// <reference path="globals/angular/index.d.ts" />
+/// <reference path="globals/bootstrap/index.d.ts" />
+/// <reference path="globals/jquery/index.d.ts" />
+/// <reference path="modules/lodash/index.d.ts" />
