31cb361ece67b0646422cc78c3e4daa391a34c11
joonhwan <joonhwan.lee@gmail.com>
2016-07-14 07:56:21 +0900

    added module 3

---
+++ angular-directives/src/module_3/link_functions/main.ts
@@ -0,0 +1,18 @@
+/// <reference path="../../app.d.ts" />
+import 'bootstrap/dist/css/bootstrap.min.css';
+import * as angular from 'angular';
+
+class MainCtrl {
+  static className = 'mainCtrl';
+  
+  message:string = "Hello Joonhwan";
+  
+  constructor() {
+  }
+}
+
+
+angular
+  .module('app', [])
+  .controller(MainCtrl.className, MainCtrl)
+  ;
