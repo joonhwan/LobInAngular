49789f4f4b57ddc79c213498d3e2b001c283b0d9
joonhwan <joonhwan.lee@gmail.com>
2016-07-22 16:25:22 +0900

    react-for-angular : tsx/jsx개념적용직전

--- react-flux-for-angular-developer/in-react/src/app/components/forum.ts
+++ react-flux-for-angular-developer/in-react/src/app/components/forum.ts
@@ -2,11 +2,36 @@ import * as React from 'react';
 import { ForumHeader } from './forumHeader';
 
 export const Forum = React.createClass({
+
+  getInitialState() {
+    return {
+      allAnswers: [
+        {
+          id: 1,
+          body: "Isn't that about time travel?",
+          correct: false
+        },
+        {
+          id: 2,
+          body: "React and Flux are a tool and methodologies for building the front end of web applications.",
+          correct: false
+        },
+        {
+          id: 3,
+          body: "React is a synonym for 'respond'",
+          correct: false
+        }
+      ]
+    };
+  },
+
   render() {
+
     return React.createElement(
       'div',
        null,
-       React.createElement(ForumHeader, null)
+       React.createElement(ForumHeader, { answers: this.state.allAnswers })
        );
+
   }
 });
--- react-flux-for-angular-developer/in-react/src/app/components/forumHeader.ts
+++ react-flux-for-angular-developer/in-react/src/app/components/forumHeader.ts
@@ -1,15 +1,20 @@
 import * as React from 'react';
 
 export const ForumHeader = React.createClass({
+
   render() {
+
+    let answerCount = this.props.answers ? this.props.answers.length : 0;
+
     return React.createElement(
       'nav',
       { className: 'navbar navbar-default' },
       React.createElement(
         'a',
-        { href:'#', className:'navbar-brand'},
-        'React Forum'
+        { href: '#', className: 'navbar-brand' },
+        'React Forum' 
       )
     );
   }
+
 });
