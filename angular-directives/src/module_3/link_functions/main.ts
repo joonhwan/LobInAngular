b27aaf8d18d50c28a48be94a5ffa890e207ba43b
joonhwan <joonhwan.lee@gmail.com>
2016-07-26 00:33:29 +0900

    ng2directive : spacebar-support finished

--- angular-directives/src/module_3/link_functions/main.ts
+++ angular-directives/src/module_3/link_functions/main.ts
@@ -5,32 +5,81 @@ import * as angular from 'angular';
 class MainCtrl {
   static className = 'mainCtrl';
 
-  message:string = "Hello Joonhwan";
+  message: string = "Hello Joonhwan";
+  messages: {text:string}[];
 
   constructor() {
+    this.messages = [];
+  }
+
+  handlePause(e) {
+    console.log('called handlePause() ' + e);
+    this.messages.push({text:'paused'});
   }
 }
 
+function namedCtor<T>(aClass: {new():T;}): Object {
+  let className = (aClass as any).className as string;
+  let ctorObj = {};
+  ctorObj[className] = aClass;
+  return ctorObj;
+}
+
+function namedFactory<T extends ng.IDirective>(aClass: {new(...args:any[]): T; }): Object {
+    let factory = (args) => new aClass(args);
+    factory.$inject = aClass.$inject || [];
+
+    let directiveName = (aClass as any).className as string;
+    let factoryObj = {};
+    factoryObj[directiveName] = factory;
+    return factoryObj;
+}
+
+class SpacebarSupport implements ng.IDirective {
+  static className = "spacebarSupport";
+  constructor() {
+
+  }
+  restrict = 'A';
+  link(scope, element, attrs) {
+    $('body').on("keypress", function (evt) {
+            console.log('keypressed ' + element[0]);
+            var videoElement = element[0] as HTMLVideoElement;
+            if (videoElement.paused) {
+              videoElement.play();
+            } else {
+              videoElement.pause();
+            }
+          });
+  }
+}
+
+class EventPaused implements ng.IDirective {
+  static className = 'eventPaused';
+  static $inject = ['$parse']
+
+  constructor(private $parse:ng.IParseService) {
+    console.log('created event paused directive...');
+  }
+
+  restrict = 'A';
+  // scope = {
+  //   eventPaused: '&'
+  // };
+  link(scope, elem, attrs) {
+    console.log('EventPause linked to ' + elem.nodeName);
+    let fn = this.$parse(attrs[EventPaused.className]);
+    elem.on('pause', function(e) {
+      scope.$apply(function() {
+        fn(scope, {event:e});
+      })
+    });
+  }
+}
 
 angular
   .module('app', [])
-  .controller(MainCtrl.className, MainCtrl)
-  .directive("spacebarSupport", () => {
-
-    return {
-      restrict: 'A',
-      //link: (scope: ng.IScope, element: JQuery, attrs: ng.IAttributes, controller: {}, transclude: ng.ITranscludeFunction) => {
-      link: (scope, element, attrs) => {
-        $('body').on("keypress", function(evt) {
-          console.log('keypressed ' + element[0]);
-          var videoElement = element[0] as any;
-          if(videoElement.paused) {
-            videoElement.play();
-          } else {
-            videoElement.pause();
-          }
-        });
-      }
-    } as ng.IDirective;
-  })
+  .controller(namedCtor(MainCtrl))
+  .directive(namedFactory(SpacebarSupport))
+  .directive(namedFactory(EventPaused))
   ;
